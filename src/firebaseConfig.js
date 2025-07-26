832b7c01333738959bca7903ce6f885b2e55324c
giovannironzino <135839067+giovannironzino@users.noreply.github.com>
2025-07-26 02:43:32 -0300

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Dentro do arquivo: src/firebaseConfig.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyARV0BcKI_yMIwY-Exc_-4hoELL6RAfKsc",
+  authDomain: "transcriber-f4198.firebaseapp.com",
+  projectId: "transcriber-f4198",
+  storageBucket: "transcriber-f4198.firebaseapp.com",
+  messagingSenderId: "308749024512",
+  appId: "1:308749024512:web:9658f467ae3fc55094c7e5",
+  measurementId: "G-28FNTTKY9M"
+};
